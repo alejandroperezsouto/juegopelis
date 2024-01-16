116692214e8b786afdb731cb62780b4698046b4e
alejandroperezsouto <154904795+alejandroperezsouto@users.noreply.github.com>
2024-01-16 16:06:03 +0100

    f.v

--- main.js
+++ main.js
@@ -3,116 +3,116 @@ let peli = [
 		"Soy el rey del mundo.",
 		"Estás temblando.",
 		"La vida es un juego de azar..",
-		["Titanic", "Taitanic", "La del barco "]
+		["titanic", "taitanic", "La del barco "]
 	],
 	[
 		"Yo soy tu padre.",
 		"Tengo un mal presentimiento.",
 		"Usa la fuerza.",
-		["Starwars", "Star wars", "La guerra de las galaxias"]
+		["starwars", "star wars", "la guerra de las galaxias"]
 	],
 	[
 		"Huid insensatos.",
 		"Al amanecer del tercer día, mira al este.",
 		"Comarca, Bolsón.",
-		["El Señor de los Anillos", "Lord of the rings", "El señor de los anillos"]
+		["el Señor de los Anillos", "lord of the rings", "El señor de los anillos"]
 	],
 	[
 		"He visto cosas que jamás creeríais.",
 		"Más allá de las puertas de Tanjauser.",
 		"Todos estos momentos se perderán en el tiempo como lágrimas en la lluvia.",
-		["Blade Runner", "Bladerunner", "Blade runner"]
+		["blade Runner", "bladerunner", "Blade runner"]
 	],
 	[
 		"Tus carencias como hijo son mis defectos como padre.",
 		"Sangre y arena.",
 		"Ahora somos libres.",
-		["Gladiator", "Gladiador", "Gladiador"]
+		["gladiator", "gladiador", "gladiador"]
 	],
 	[
 		"Voy a hacerle una oferta que no podrá rechazar.",
 		"Un hombre que no pasa tiempo con su familia no puede ser considerado hombre.",
 		"Guarda tus amigos cerca y a tus enemigos aún más cerca.",
-		["El Padrino", "El Padríno", "El Padriño"]
+		["el Padrino", "el Padríno", "the goodfather"]
 	],
 	[
 		"Que la Fuerza te acompañe.",
 		"Ayuda me, Obi-Wan Kenobi; eres mi única esperanza.",
 		"No, yo soy tu padre.",
-		["Star Wars", "Star wars", "La guerra de las galaxias"]
+		["star Wars", "star wars", "la guerra de las galaxias"]
 	],
 	[
 		"Eres un mago, Harry.",
 		"Después de todo este tiempo? -Siempre.",
 		"Las cosas que perdemos siempre regresan a nosotros de alguna manera.",
-		["Harry Potter", "Harry potter", "Las Reliquias de la Muerte"]
+		["harry Potter", "harry potter", "las Reliquias de la Muerte"]
 	],
 	[
 		"¡Hasta la vista, baby!",
 		"Ven conmigo si quieres vivir.",
 		"Yo volveré.",
-		["Terminator", "El Exterminador", "Exterminador del Futuro"]
+		["terminator", "el Exterminador", "exterminador del Futuro"]
 	],
 	[
 		"Houston, tenemos un problema.",
 		"La vida se abre camino.",
 		"En el espacio nadie puede oír tus gritos.",
-		["Apolo 13", "Apollo 13", "Misión a la Luna"]
+		["apolo 13", "apollo 13", "misión a la Luna"]
 	],
 	[
 		"Hasta el infinito y más allá.",
 		"Siempre estaré contigo.",
 		"Al infinito y más allá.",
-		["Toy Story", "Toy story", "La historia de los juguetes"]
+		["toy Story", "toy story", "la historia de los juguetes"]
 	],
 	[
 		"¿Por qué tan serio?",
 		"Creo que lo que no te mata, simplemente te hace más raro.",
 		"Pongamos una sonrisa en esa cara.",
-		["El Caballero de la Noche", "El caballero oscuro", "El caballero oscuro"]
+		["el Caballero de la Noche", "el caballero oscuro", "el caballero oscuro"]
 	],
 	[
 		"Aquí tienes mirándote, chaval.",
 		"Siempre nos quedará París.",
 		"Louis, creo que este es el comienzo de una hermosa amistad.",
-		["Casablanca", "Casablanca", "La ciudad de la alegría"]
+		["casablanca", "casablanca", "la ciudad de la alegría"]
 	],
 	[
 		"No hay lugar como el hogar.",
 		"Te atraparé, mi preciosa, ¡y también a tu perrito!",
 		"No hagas caso al hombre que está detrás de la cortina.",
-		["El Mago de Oz", "El mago de Oz", "El Mago de Oz"]
+		["el Mago de Oz", "el mago de Oz", "el Mago de Oz"]
 	],
 	[
 		"Que la suerte esté siempre de tu lado.",
 		"¡Felices Juegos del Hambre! ¡Y que la suerte esté siempre de tu lado!",
 		"¡Me ofrezco como voluntario!",
 		[
-			"Los Juegos del Hambre",
-			"Los juegos del hambre",
-			"Los juegos de la hambruna"
+			"los Juegos del Hambre",
+			"los juegos del hambre",
+			"los juegos de la hambruna"
 		]
 	],
 	[
 		"Elemental, mi querido Watson.",
 		"El juego está en marcha.",
 		"Es elemental, mi querido Watson.",
-		["Sherlock Holmes", "Sherlock holmes", "Las aventuras de Sherlock Holmes"]
+		["sherlock Holmes", "sherlock holmes", "las aventuras de sherlock holmes"]
 	],
 	[
 		"¡Aquí está Johnny!",
 		"Todo trabajo y nada de diversión hacen de Jack un chico aburrido.",
 		"¡Wendy, ya estoy en casa!",
-		["El Resplandor", "El resplandor", "El iluminado"]
+		["el Resplandor", "el resplandor", "el iluminado"]
 	],
 	[
 		"Volveré.",
 		"¡Hasta la vista, baby!",
 		"Ven conmigo si quieres vivir.",
 		[
-			"Terminator 2: Juicio Final",
-			"Terminator 2: Día del Juicio",
-			"El Exterminador 2: Día del juicio"
+			"terminator 2: juicio final",
+			"terminator 2",
+			"el Exterminador 2: Día del juicio"
 		]
 	],
 	[
@@ -120,83 +120,82 @@ let peli = [
 		"Voy a hacerle una oferta que no podrá rechazar.",
 		"¡Lo juro por Dios!",
 		[
-			"Algunos hombres buenos",
-			"Unos pocos hombres buenos",
-			"Algunos hombres honrados"
+			"algunos hombres buenos",
+			"unos pocos hombres buenos",
+			"algunos hombres honrados"
 		]
 	],
 	[
 		"Veo gente muerta.",
 		"¡Están aquí!",
 		"¡Ya están aquí! ¡Tú serás el siguiente!",
-		["El Sexto Sentido", "El sexto sentido", "El sexto sentido"]
+		["el Sexto Sentido", "sexto sentido", "El sexto sentido"]
 	],
 	[
 		"No existe el amor en el mundo.",
 		"Vete, olvídame. Por favor, olvídame, Faline.",
 		"Siempre recordaré que me encontré con bambi.",
-		["Bambi", "El pequeño ciervo", "Bambi"]
+		["bambi", "el pequeño ciervo", "bambi"]
 	],
 	[
 		"Mi tesoro.",
 		"No te dejaré ir, Jack.",
 		"Solo podemos abrazarnos y esperar.",
-		["Titanic", "Taitanic", "La tragedia del mar"]
+		["titanic", "taitanic", "la tragedia del mar"]
 	],
 	[
 		"Que se haga la luz.",
 		"Simba, tú aún no estás listo para ser rey.",
 		"¿Vas a ignorar mis órdenes?",
-		["El Rey León", "El rey león", "El monarca león"]
+		["el Rey León", "el rey león", "el monarca león"]
 	],
 	[
 		"Yo amo el olor del napalm por la mañana.",
 		"He matado a muchas personas.",
 		"No puedo morir, ¡no puedo morir!",
-		["Apocalypse Now", "Apocalipsis ahora", "El día del fin"]
+		["apocalypse Now", "apocalipsis ahora", "el día del fin"]
 	],
 	[
 		"No te muevas, o te mueres.",
 		"Atrévete, no tienes nada que perder.",
 		"¿Y si me abrazas hasta que me sienta mejor?",
-		["Kill Bill", "Kill Bill: Volumen 1", "Kill Bill: La venganza"]
+		["kill Bill", "Kill Bill: Volumen 1", "Kill Bill: La venganza"]
 	],
 	[
 		"Los sueños, sueños son.",
 		"Nada es ni bueno ni malo, solo lo es el pensamiento.",
 		"Buenos amigos, buenos libros y una conciencia tranquila: esa es la vida ideal.",
-		["Romeo y Julieta", "Romeo y Julieta", "Romeo y Julieta"]
+		["romeo y julieta", "Romeo y Julieta", "Romeo y Julieta"]
 	],
 	[
 		"Solo hay una cosa que el poder no puede comprar: el tiempo.",
 		"El pasado ya no se puede cambiar. El futuro aún está en tu poder.",
 		"El tiempo es demasiado lento para aquellos que esperan... demasiado rápido para aquellos que temen...",
-		["Tiempos modernos", "Modern Times", "Tiempos de cambio"]
+		["tiempos modernos", "modern Times", "tiempos de cambio"]
 	],
 	[
 		"Para aquellos que luchan por ella, la vida tiene un sabor que los protegidos nunca conocerán.",
 		"Cierro los ojos para no ver lo que pasa a mi alrededor.",
 		"Un hombre puede ser destruido pero no derrotado.",
-		["Adiós a las armas", "Farewell to Arms", "Despedida de armas"]
+		["adiós a las armas", "farewell to Arms", "despedida de armas"]
 	],
 	[
 		"El arte de perder no es difícil de aprender.",
 		"Nadie osó molestar al sonido de mi pie.",
 		"No puedo evitarlo, es emocionante. Se siente bien estar perdido en la dirección correcta.",
-		["Lost in Translation", "Perdidos en Tokio", "Traducción perdida"]
+		["lost in translation", "perdidos en Tokio", "traducción perdida"]
 	],
 	[
 		"No te rindas, no importa lo que suceda, no te rindas.",
 		"Siempre he confiado en la bondad de los extraños.",
 		"Allá voy, a ser famoso... y morir joven.",
 		[
-			"Lo que el viento se llevó",
+			"o que el viento se llevó",
 			"Lo que el viento se llevó",
 			"Gone with the Wind"
 		]
 	]
 ];
-
 let npeli = 0;
 let npista = 0;
 let puntos = 0;
